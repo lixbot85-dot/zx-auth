6abb1a28af2882a35eff5ea46f87138780f95b8f
lixbot85-dot <lixbot85@gmail.com>
2026-04-02 15:39:22 -0300

    Update server.py

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ LIBRARY_PATH = os.path.join(BASE_PATH, "library")
 # ===== HOME =====
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 # ===== FILE EXPLORER (JSON + HTML SUPPORT) =====
 @app.route("/files/", defaults={"req_path": ""})
