1b410e6875c2d399711028396746cbc3de931a3b
lixbot85-dot <lixbot85@gmail.com>
2026-04-07 12:03:13 -0300

    Create jsonloader.lua

---
+++ files/APIs/jsonloader.lua
@@ -0,0 +1,36 @@
+-- <-Json Loader V1-> --
+local JsonLoader = {}
+
+function JsonLoader.Load(url)
+    local HttpService = game:GetService("HttpService")
+    
+    local success, response = pcall(function()
+        return game:HttpGet(url)
+    end)
+    if not success then
+        warn("[JsonLoader] Não foi possível acessar URL:", response)
+        return {}
+    end
+
+    local data = {}
+    local decodeSuccess, res = pcall(function()
+        return HttpService:JSONDecode(response)
+    end)
+    if decodeSuccess then
+        data = res
+    else
+        warn("[JsonLoader] Erro ao decodificar JSON:", res)
+        return {}
+    end
+
+    local list = {}
+    if type(data) == "table" then
+        for _, item in pairs(data) do
+            table.insert(list, item)
+        end
+    end
+
+    return list
+end
+
+return JsonLoader
